e1761054d237b50e2fcd3ffbca38ca9e8dc7347d
Host <host.michael@gmail.com>
2015-08-11 16:29:06 +0200

    Update index.js

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,11 +1,40 @@
+var settings;
+    
+function Settings() {
+if ((typeof Camera !== "undefined")) {
+this.destinationType = Camera.DestinationType.FILE_URI; // cameraOptions: destinationType
+this.sourceType = Camera.PictureSourceType.CAMERA; 
+this.mediaType = Camera.MediaType.PICTURE; // cameraOptions: mediaType
+}
+this.quality = 100; // cameraOptions: quality
+this.targetWidth = 600; // cameraOptions: targetWidth
+this.targetHeight = 1000; // cameraOptions: targetHeight
+this.allowEdit = false; // cameraOptions: allowEdit
+this.correctOrientation = false;
+//this.cameraDirection = 0;
+this.positionPaysage = true;
+this.encodingType = (typeof Camera !== "undefined") ? Camera.EncodingType.JPEG : 0; // cameraOptions: encodingType
+this.saveToPhotoAlbum = false; // cameraOptions: saveToPhotoAlbum
+this.popoverOptions = new CameraPopoverOptions(100, 100, 100, 100, Camera.PopoverArrowDirection.ARROW_DOWN); // cameraOptions: popoverOptions
+}
+
 function onLoadCamera() {alert('onloadCamera');
-	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
-	
-function geolocationSuccess(position) {alert(position.coords.latitude);}
 
-function geolocationError(error) {}
-
+document.addEventListener("deviceready", onDeviceReady, false);
 
 }
 
+function onDeviceReady() {
+    
+    
+    $("#open_camera_button").bind ("click", onCapture);
+    $("#open_lib_button").bind ("click", onCapture);
+    $("#open_alb_button").bind ("click", onCapture);
+    
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("offline", onOffline, false)
+}
 
+function onCapture(e) { alert("onCapture");
+
+}
