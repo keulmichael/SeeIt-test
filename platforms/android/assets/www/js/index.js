8ac3b766e12e46300cfb3d9df45418d2a4efd1ea
Host <host.michael@gmail.com>
2015-08-11 17:32:50 +0200

    Update index.js

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,25 +1,10 @@
 function onLoadCamera() {
 
-	var networkState = navigator.network.connection.type;
-
-        var states = {};
-        states[Connection.UNKNOWN] = 'Connexion inconnue';
-        states[Connection.ETHERNET] = 'Connexion Ethernet';
-        states[Connection.WIFI] = 'Connexion WiFi';
-        states[Connection.CELL_2G] = 'Connexion 2G';
-        states[Connection.CELL_3G] = 'Connexion 3G';
-        states[Connection.CELL_4G] = 'Connexion 4G';
-        states[Connection.NONE] = 'Pas de connexion réseau';
-alert(states[networkState]);
-if (states[networkState] == 'Pas de connexion réseau') {
-        document.getElementById("problemeReseau").innerHTML="<font color='red' size='2'>Absence de réseau. Veuillez fermer l'application et l'ouvrir à nouveau.</font>";}
-
     document.addEventListener("deviceready", onDeviceReady, false);
     
     settings = new Settings();
     
-var geocoder;
-geocoder = new google.maps.Geocoder();
+
 }
 
 var settings;
@@ -47,7 +32,7 @@ this.popoverOptions = new CameraPopoverOptions(100, 100, 100, 100, Camera.Popove
 
 
 
-function onDeviceReady() {
+function onDeviceReady() {alert('onDeviceReady');
     
     
     $("#open_camera_button").bind ("click", onCapture);
@@ -87,19 +72,7 @@ if (acceleration.x>1 || acceleration.x<-1 && acceleration.y<9 && acceleration.z>
             return;
     }
     
-    navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : settings.quality,
-                                                                    destinationType : settings.destinationType,
-                                                                    sourceType : settings.sourceType,
-                                                                    allowEdit : settings.allowEdit,
-                                                                    encodingType : settings.encodingType,
-                                                                    targetWidth : settings.targetWidth,
-                                                                    targetHeight : settings.targetHeight,
-                                                                    mediaType: settings.mediaType,
-                                                                    saveToPhotoAlbum : settings.saveToPhotoAlbum,
-                                                                    correctOrientation: settings.correctOrientation,
-								    cameraDirection: settings.cameraDirection,
-                                                                    popoverOptions : settings.popoverOptions
-                                                                  });
+    navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : 100 });
 
 
 }
@@ -135,19 +108,7 @@ if (acceleration.x<9 || acceleration.x>-9 && acceleration.y<0 || acceleration.y>
             return;
     }
     
-    navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : settings.quality,
-                                                                    destinationType : settings.destinationType,
-                                                                    sourceType : settings.sourceType,
-                                                                    allowEdit : settings.allowEdit,
-                                                                    encodingType : settings.encodingType,
-                                                                    targetWidth : settings.targetWidth,
-                                                                    targetHeight : settings.targetHeight,
-                                                                    mediaType: settings.mediaType,
-                                                                    saveToPhotoAlbum : settings.saveToPhotoAlbum,
-                                                                    correctOrientation: settings.correctOrientation,
-								    cameraDirection: settings.cameraDirection,
-                                                                    popoverOptions : settings.popoverOptions
-                                                                  });
+    navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : 100});
 
 
 		}
