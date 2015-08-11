4d2dac088fca11df8b619ef72baf3c2d6b0ad601
Host <host.michael@gmail.com>
2015-08-11 16:09:58 +0200

    Update index.js

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,29 +1,10 @@
-
-
-    
-function Settings() {
-if ((typeof Camera !== "undefined")) {
-this.destinationType = Camera.DestinationType.FILE_URI; // cameraOptions: destinationType
-this.sourceType = Camera.PictureSourceType.CAMERA; 
-this.mediaType = Camera.MediaType.PICTURE; // cameraOptions: mediaType
-}
-this.quality = 100; // cameraOptions: quality
-this.targetWidth = 600; // cameraOptions: targetWidth
-this.targetHeight = 1000; // cameraOptions: targetHeight
-this.allowEdit = false; // cameraOptions: allowEdit
-this.correctOrientation = false;
-//this.cameraDirection = 0;
-this.positionPaysage = true;
-this.encodingType = (typeof Camera !== "undefined") ? Camera.EncodingType.JPEG : 0; // cameraOptions: encodingType
-this.saveToPhotoAlbum = false; // cameraOptions: saveToPhotoAlbum
-this.popoverOptions = new CameraPopoverOptions(100, 100, 100, 100, Camera.PopoverArrowDirection.ARROW_DOWN); // cameraOptions: popoverOptions
-}
-
-var settings;
-	
-
 function onLoadCamera() {alert('onloadCamera');
+	navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
 	
+function geolocationSuccess(position) {alert(position.coords.latitude);}
+
+function geolocationError(error) {}
+
 	var networkState = navigator.network.connection.type;
 
         var states = {};
@@ -45,6 +26,31 @@ if (states[networkState] == 'Pas de connexion réseau') {
 var geocoder;
 geocoder = new google.maps.Geocoder();
 }
+
+var settings;
+    
+function Settings() {
+if ((typeof Camera !== "undefined")) {
+this.destinationType = Camera.DestinationType.FILE_URI; // cameraOptions: destinationType
+this.sourceType = Camera.PictureSourceType.CAMERA; 
+this.mediaType = Camera.MediaType.PICTURE; // cameraOptions: mediaType
+}
+this.quality = 100; // cameraOptions: quality
+this.targetWidth = 600; // cameraOptions: targetWidth
+this.targetHeight = 1000; // cameraOptions: targetHeight
+this.allowEdit = false; // cameraOptions: allowEdit
+this.correctOrientation = false;
+//this.cameraDirection = 0;
+this.positionPaysage = true;
+this.encodingType = (typeof Camera !== "undefined") ? Camera.EncodingType.JPEG : 0; // cameraOptions: encodingType
+this.saveToPhotoAlbum = false; // cameraOptions: saveToPhotoAlbum
+this.popoverOptions = new CameraPopoverOptions(100, 100, 100, 100, Camera.PopoverArrowDirection.ARROW_DOWN); // cameraOptions: popoverOptions
+}
+
+
+	
+
+
 
 function onDeviceReady() {
     
