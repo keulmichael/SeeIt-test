1fa6b3ba096f4f4a8801519349073a7664cb76de
Host <host.michael@gmail.com>
2015-08-12 23:58:22 +0200

    Update index.js

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -19,17 +19,66 @@ this.popoverOptions = new CameraPopoverOptions(100, 100, 100, 100, Camera.Popove
 document.addEventListener('deviceready', onDeviceReady, false);
 
 
- function onDeviceReady() {
-    $("#open_camera_button").bind ("click", onCapture);
-    $("#open_lib_button").bind ("click", onCapture);
-    $("#open_alb_button").bind ("click", onCapture);
- }
+function onDeviceReady (){
+	
+//	var networkState = navigator.network.connection.type;
 
+//        var states = {};
+//        states[Connection.UNKNOWN] = 'Connexion inconnue';
+//        states[Connection.ETHERNET] = 'Connexion Ethernet';
+//        states[Connection.WIFI] = 'Connexion WiFi';
+//        states[Connection.CELL_2G] = 'Connexion 2G';
+//        states[Connection.CELL_3G] = 'Connexion 3G';
+ //       states[Connection.CELL_4G] = 'Connexion 4G';
+ //       states[Connection.NONE] = 'Pas de connexion r&eacute;seau';
+
+//if (states[networkState] == 'Pas de connexion r&eacute;seau') {
+//        document.getElementById("problemeReseau").innerHTML="<font color='red' size='2'>Absence de r&eacute;seau. Veuillez fermer l'application et l'ouvrir &agrave; nouveau lorsque l'appareil sera connect&eacute;.</font>";}
+
+// else {   
+$("#open_camera_button").bind ("click", onCapture);
+$("#open_lib_button").bind ("click", onCapture);
+$("#open_alb_button").bind ("click", onCapture);
+// }
+}
+	
+ 
+
+	
+//var geocoder;
+//geocoder = new google.maps.Geocoder();
+
+///////////////////////////////////////////////////////////////////////////////////////	
 function onCapture(e) { 
-var settings;
-settings = new Settings();
-		
-navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : settings.quality,
+	var settings
+settings = new Settings();  
+if (settings.positionPaysage==false)
+	{
+//	navigator.accelerometer.getCurrentAcceleration(accelerometerSuccessPortrait, accelerometerErrorPortrait);
+
+//	function accelerometerSuccessPortrait(acceleration) {
+//if (acceleration.x>1 || acceleration.x<-1 && acceleration.y<9 && acceleration.z>1 || acceleration.z<-1 ){
+//   alert("Veuillez tenir l\'appareil en mode portrait.\nLe blocage en mode portrait peut être activé dans les options.");
+//		}
+
+//	else {
+    var callerId = getTargetId(e, "a");
+    
+    switch (callerId) {
+        case "open_camera_button":
+            settings.sourceType = Camera.PictureSourceType.CAMERA;
+            break;
+        case "open_lib_button":
+            settings.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+            break;
+        case "open_alb_button":
+            settings.sourceType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+            break;
+        default:
+            return;
+    }
+    
+    navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : settings.quality,
                                                                     destinationType : settings.destinationType,
                                                                     sourceType : settings.sourceType,
                                                                     allowEdit : settings.allowEdit,
@@ -43,7 +92,68 @@ navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : settin
                                                                     popoverOptions : settings.popoverOptions
                                                                   });
 
-			  
+
+//}
+//}
+//function accelerometerErrorPortrait() {
+//   alert('Veuillez activer l\'accéléromètre');
+//};								  
+}
+
+else
+{
+//navigator.accelerometer.getCurrentAcceleration(accelerometerSuccessPaysage, accelerometerErrorPaysage);
+
+//function accelerometerSuccessPaysage(acceleration) {
+//if (acceleration.x<9 || acceleration.x>-9 && acceleration.y<0 || acceleration.y>1 && acceleration.z<0 || acceleration.y>1){
+//    alert("Veuillez tenir l'appareil en mode paysage.\nLe blocage en mode portrait peut être activé dans les options.");
+//		}
+
+//	else {
+    var callerId = getTargetId(e, "a");
+    
+    switch (callerId) {
+        case "open_camera_button":
+            settings.sourceType = Camera.PictureSourceType.CAMERA;
+            break;
+        case "open_lib_button":
+            settings.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+            break;
+        case "open_alb_button":
+            settings.sourceType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
+            break;
+        default:
+            return;
+    }
+    
+    navigator.camera.getPicture(onCaptureSuccess, onCaptureError, { quality : settings.quality,
+                                                                    destinationType : settings.destinationType,
+                                                                    sourceType : settings.sourceType,
+                                                                    allowEdit : settings.allowEdit,
+                                                                    encodingType : settings.encodingType,
+                                                                    targetWidth : settings.targetWidth,
+                                                                    targetHeight : settings.targetHeight,
+                                                                    mediaType: settings.mediaType,
+                                                                    saveToPhotoAlbum : settings.saveToPhotoAlbum,
+                                                                    correctOrientation: settings.correctOrientation,
+								    cameraDirection: settings.cameraDirection,
+                                                                    popoverOptions : settings.popoverOptions
+                                                                  });
+
+
+		}
+	};
+
+
+//function accelerometerErrorPaysage() {
+//    alert('Veuillez activer l\'acc&eacute;l&eacute;rom&egrave;tre');
+//};
+		
+//}
+//}
+
+	
+///////////////////////////////////////////////////////////////////////////////////////
 function onCaptureSuccess(imageData) {
 	
 	var num = document.getElementById("num").value;
@@ -79,44 +189,63 @@ ftQuali.upload(imageData, fichieruploadQuali, winQuali, failQuali, options);
  
 
 
-require(["dojo/request"], function(request){    
-request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x=&y=&adress=').then(function(response271){ 	
-navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+//require(["dojo/request"], function(request){    
+//request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x=&y=&adress=').then(function(response271){ 	
+//navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
 	
-function geolocationSuccess(position) {alert("Position enregistrée !\n"+position.coords.latitude+", "+position.coords.longitude);
-require(["dojo/request"], function(request){    
-var num = document.getElementById("num").value;	
-var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-geocoder.geocode({'latLng': latlng}, function(results, status) {
-	if (status == google.maps.GeocoderStatus.OK) {
-if (results[1]) {
-      request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x='+position.coords.latitude+'&y='+position.coords.longitude+'&adress='+results[1].formatted_address).then(function(response276){document.getElementById("affichFormattedAddress").innerHTML="<font size=2 color=grey>"+results[1].formatted_address+"</font>";}) ;
-      }}
-})	
+//function geolocationSuccess(position) {alert("Position enregistrée !\n"+position.coords.latitude+", "+position.coords.longitude);
+//require(["dojo/request"], function(request){    
+//var num = document.getElementById("num").value;	
+//var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//geocoder.geocode({'latLng': latlng}, function(results, status) {
+//	if (status == google.maps.GeocoderStatus.OK) {
+//if (results[1]) {
+ //     request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x='+position.coords.latitude+'&y='+position.coords.longitude+'&adress='+results[1].formatted_address).then(function(response276){document.getElementById("affichFormattedAddress").innerHTML="<font size=2 color=grey>"+results[1].formatted_address+"</font>";}) ;
+//      }}
+//})	
 
-    })
+//    })
+//}
+
+//function geolocationError(error) {
+//switch(error.code){
+//    case error.PERMISSION_DENIED:
+//      alert("L'utilisateur n'a pas autorisé l'accés à sa position");
+//      break;      
+ //   case error.POSITION_UNAVAILABLE:
+ //     alert("L'emplacement de l'utilisateur n'a pas pu être déterminé");
+ //     break;
+ //   case error.TIMEOUT:
+ //     alert("Le service n'a pas répondu à temps");
+  //    break;
+  //  }
+//}
+//	}) 	
+//})	
+
 }
 
-function geolocationError(error) {
-switch(error.code){
-    case error.PERMISSION_DENIED:
-      alert("L'utilisateur n'a pas autorisé l'accés à sa position");
-      break;      
-    case error.POSITION_UNAVAILABLE:
-      alert("L'emplacement de l'utilisateur n'a pas pu être déterminé");
-      break;
-    case error.TIMEOUT:
-      alert("Le service n'a pas répondu à temps");
-      break;
+
+function onCaptureError(message) {alert('erreur capture'); }
+
+
+
+
+
+function getTargetId(event, tagName) {
+    var target = (event.target.tagName == tagName)
+                    ? event.target
+                    : $(event.target).closest(tagName)[0]
+    return target.id;
+}
+
+function getElement(element) {
+    
+    if(typeof(element) == "string") {
+    
+        element = document.getElementById(element);
     }
-}
-	}) 	
-})	
-
-}
-
-
-function onCaptureError(message) {alert(message); }
-
+    
+    return element;
 }
 
