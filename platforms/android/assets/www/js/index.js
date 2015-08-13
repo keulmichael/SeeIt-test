cca3b10066de39b31c569a6170482f42cba3a350
Host <host.michael@gmail.com>
2015-08-13 18:07:17 +0200

    Update index.js

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -193,7 +193,7 @@ var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.lo
 geocoder.geocode({'latLng': latlng}, function(results, status) {
 if (status == google.maps.GeocoderStatus.OK) {
 if (results[1]) {
-     request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x='+position.coords.latitude+'&y='+position.coords.longitude+'&adress='+results[1].formatted_address).then(function(response276){document.getElementById("affichFormattedAddress").innerHTML="<font size=2 color=grey>"+results[1].formatted_address+"</font>";alert(results[1].formatted_address)}) ;
+     request.get('http://www.appliseeit.com/mobile/record_gps.php?num='+num+'&x='+position.coords.latitude+'&y='+position.coords.longitude+'&adress='+results[1].formatted_address).then(function(response276){document.getElementById("affichFormattedAddress").innerHTML="<font size=2 color=grey>"+results[1].formatted_address+"</font>";}) ;
 }}
 })	
 
