a0bc9f064de3f331d64a7644e2723e1465676f3e
Host <host.michael@gmail.com>
2015-08-17 19:14:16 +0200

    Update changeOrientation.js

--- platforms/android/assets/www/js/changeOrientation.js
+++ platforms/android/assets/www/js/changeOrientation.js
@@ -1,16 +1,24 @@
+	document.getElementById('containerAccueil').style.height = '170px';
+	document.getElementById('icone-seeit.png').style.height = '150px';
+      	document.getElementById('photoaccueil_1').style.width = '70%';
+      	document.getElementById('photoaccueil_2').style.width = '70%';
+      	document.getElementById('photoaccueil_3').style.width = '70%';
+      	document.getElementById('pubaccueil_1').style.width = '70%';
+      	document.getElementById('pubaccueil_2').style.width = '70%';
+      	
 function listenerTailleEcran(requeteMedia) {
 	var orientationPortrait = document.getElementById('orientationPortrait');
 	if (requeteMedia.matches) { 
 	document.getElementById('containerAccueil').style.height = '170px';
-	document.images["icone-seeit.png"].height = '150px';	
-      	document.images["photoaccueil_1"].width = '70%';
-      	document.images["photoaccueil_2"].width = '70%';
-      	document.images["photoaccueil_3"].width = '70%';
-      	document.images["pubaccueil_1"].width = '70%';
-      	document.images["pubaccueil_2"].width = '70%';
+	document.getElementById('icone-seeit.png').style.height = '150px';
+      	document.getElementById('photoaccueil_1').style.width = '70%';
+      	document.getElementById('photoaccueil_2').style.width = '70%';
+      	document.getElementById('photoaccueil_3').style.width = '70%';
+      	document.getElementById('pubaccueil_1').style.width = '70%';
+      	document.getElementById('pubaccueil_2').style.width = '70%';
 	} else { 
 	document.getElementById('containerAccueil').style.height = '170px';
-	document.getElementById('icone-seeit.png').style.width = '150px';	
+	document.getElementById('icone-seeit.png').style.height = '150px';	
       	document.getElementById('photoaccueil_1').style.width = '40%';
       	document.getElementById('photoaccueil_2').style.width = '40%';
       	document.getElementById('photoaccueil_3').style.width = '40%';
