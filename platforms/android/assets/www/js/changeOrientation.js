a0326a7fdd8dcfd7f1bb2b62f78f1af9b3724693
Host <host.michael@gmail.com>
2015-08-17 20:04:40 +0200

    Update changeOrientation.js

--- platforms/android/assets/www/js/changeOrientation.js
+++ platforms/android/assets/www/js/changeOrientation.js
@@ -1,26 +1,23 @@
 function listenerTailleEcran(requeteMedia) {
 
+var photoaccueil_1 = document.getElementById('photoaccueil_1');
+var photoaccueil_2 = document.getElementById('photoaccueil_2');
+var photoaccueil_3 = document.getElementById('photoaccueil_3');
 
-	
 	var orientationPortrait = document.getElementById('orientationPortrait');
 	if (requeteMedia.matches) { 
 	document.getElementById('containerAccueil').style.height = '170px';
 	document.getElementById('icone-seeit.png').style.height = '150px';
-	if (document.getElementById('photoaccueil_1').naturalHeight<400){
-      	document.getElementById('photoaccueil_1').style.width = '70%';}
-      	else {
-      	document.getElementById('photoaccueil_1').style.width = '40%';}
-	if (document.getElementById('photoaccueil_2').naturalHeight<400){
-      	document.getElementById('photoaccueil_2').style.width = '70%';}
-      	else {
-      	document.getElementById('photoaccueil_2').style.width = '40%';}
-	if (document.getElementById('photoaccueil_3').naturalHeight<400){
-      	document.getElementById('photoaccueil_3').style.width = '70%';}
-      	else {
-      	document.getElementById('photoaccueil_3').style.width = '40%';}
+	if (photoaccueil_1.naturalHeight<400){photoaccueil_1.style.width = '70%';}
+      	else {photoaccueil_1.style.width = '30%';}
+	if (photoaccueil_2.naturalHeight<400){photoaccueil_2.style.width = '70%';}
+      	else {photoaccueil_2.style.width = '40%';}
+	if (photoaccueil_3.naturalHeight<400){photoaccueil_3.style.width = '70%';}
+      	else {photoaccueil_3.style.width = '40%';}
       	document.getElementById('pubaccueil_1').style.width = '70%';
       	document.getElementById('pubaccueil_2').style.width = '70%';
-	} else { 
+	} 
+	else { 
 	document.getElementById('containerAccueil').style.height = '170px';
 	document.getElementById('icone-seeit.png').style.height = '150px';	
       	document.getElementById('photoaccueil_1').style.width = '40%';
