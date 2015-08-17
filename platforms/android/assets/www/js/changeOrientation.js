e90640646174ce0be877787707f6afc37cd04c3f
Host <host.michael@gmail.com>
2015-08-17 18:48:06 +0200

    Update changeOrientation.js

--- platforms/android/assets/www/js/changeOrientation.js
+++ platforms/android/assets/www/js/changeOrientation.js
@@ -1,6 +1,6 @@
-function listenerTailleEcran(requeteMedia) {alert('requeteMedia:'+requeteMedia);
-	var orientationPortrait = document.getElementById('orientationPortrait');
-	if (requeteMedia.matches) {  
+//function listenerTailleEcran(requeteMedia) {alert('requeteMedia:'+requeteMedia);
+//	var orientationPortrait = document.getElementById('orientationPortrait');
+//	if (requeteMedia.matches) {  
 	document.getElementById('containerAccueil').style.height = '170px';
 	document.getElementById('icone-seeit.png').style.width = '150px';	
       	document.getElementById('photoaccueil_1').style.width = '70%';
@@ -8,15 +8,15 @@ function listenerTailleEcran(requeteMedia) {alert('requeteMedia:'+requeteMedia);
       	document.getElementById('photoaccueil_3').style.width = '70%';
       	document.getElementById('pubaccueil_1').style.width = '70%';
       	document.getElementById('pubaccueil_2').style.width = '70%';
-	} else {  
-	document.getElementById('containerAccueil').style.height = '170px';
-	document.getElementById('icone-seeit.png').style.width = '150px';	
-      	document.getElementById('photoaccueil_1').style.width = '40%';
-      	document.getElementById('photoaccueil_2').style.width = '40%';
-      	document.getElementById('photoaccueil_3').style.width = '40%';
-      	document.getElementById('pubaccueil_1').style.width = '40%';
-      	document.getElementById('pubaccueil_2').style.width = '40%';
-	}
+//	} else {  
+//	document.getElementById('containerAccueil').style.height = '170px';
+//	document.getElementById('icone-seeit.png').style.width = '150px';	
+//      	document.getElementById('photoaccueil_1').style.width = '40%';
+//      	document.getElementById('photoaccueil_2').style.width = '40%';
+//      	document.getElementById('photoaccueil_3').style.width = '40%';
+//      	document.getElementById('pubaccueil_1').style.width = '40%';
+//      	document.getElementById('pubaccueil_2').style.width = '40%';
+//	}
 	
 	
 //	var theimg = document.getElementById("photoaccueil_1");  
@@ -25,9 +25,9 @@ function listenerTailleEcran(requeteMedia) {alert('requeteMedia:'+requeteMedia);
 //	alert("Image Make: " + EXIF.getTag(theimg2, "Height"));
 }  
 	
-document.addEventListener('DOMContentLoaded', function(){
-	var requeteMedia = window.matchMedia("(orientation : portrait)");
-	requeteMedia.addListener(listenerTailleEcran);
+//document.addEventListener('DOMContentLoaded', function(){
+//	var requeteMedia = window.matchMedia("(orientation : portrait)");
+//	requeteMedia.addListener(listenerTailleEcran);
 	
-	listenerTailleEcran(requeteMedia);
-}, false);
+//	listenerTailleEcran(requeteMedia);
+//}, false);
