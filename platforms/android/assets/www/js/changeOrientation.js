b1e0c72fe932ff9b0de6194a652cf2072733482a
Host <host.michael@gmail.com>
2015-08-19 16:56:57 +0200

    Update changeOrientation.js

--- platforms/android/assets/www/js/changeOrientation.js
+++ platforms/android/assets/www/js/changeOrientation.js
@@ -1,5 +1,5 @@
-alert (document.getElementById('logpubaccueil_1').naturalHeight);
-alert (document.getElementById('logpubaccueil_1').naturalWidth);
+alert (document.getElementById('pubaccueil_1').naturalHeight);
+alert (document.getElementById('pubaccueil_1').naturalWidth);
 
 function listenerTailleEcran(requeteMedia) {
 
