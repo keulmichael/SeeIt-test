34d84b59a37a8576c9d8a769d35c65b0a21ab4fb
Host <host.michael@gmail.com>
2015-09-03 10:23:19 +0200

    Update websocket.js

--- platforms/android/assets/www/js/websocket.js
+++ platforms/android/assets/www/js/websocket.js
@@ -10,7 +10,7 @@ ws.onmessage = function (event) {
        case "nouvelle connexion":
        var id = Math.floor(Math.random() * 100000000000000) ;
        sessionStorage.id = id;
-show('dlg_connexion','mess');
+show('dlg_connexion');
        break;
        
        case "confirmation photo":
